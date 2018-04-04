64c10608a269706f30bed4a3746ad315d64d0a60
陈英鹏 <33784657+Heilce@users.noreply.github.com>
2018-04-04 09:25:11 +0800

    增加了扩展

--- model.py
+++ model.py
@@ -1,13 +1,5 @@
 # -*- coding:utf-8 -*-
 # author: xip tim:2018/4/2
-from flask_sqlalchemy import SQLAlchemy
-from flask import Flask
-
-app = Flask(__name__)
-# 设置数据库的地址
-app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql://root:@127.0.0.1:3306/flask_demo'
-app.secret_key = 'flask is hard'
-app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 from exts import db
 
 
