29e133b9a2c340a9e5ed63801541fefb1cc94c22
陈英鹏 <33784657+Heilce@users.noreply.github.com>
2018-04-04 09:25:32 +0800

    init commit

---
+++ config.py
@@ -0,0 +1,6 @@
+# -*- coding:utf-8 -*-
+# author: xip tim:2018/4/3
+
+SQLALCHEMY_DATABASE_URI = 'mysql://root:@127.0.0.1:3306/flask_demo'
+SECRET_KEY = 'THIS IS TOO HARD'
+SQLALCHEMY_TRACK_MODIFICATIONS = False
---
+++ exts.py
@@ -0,0 +1,5 @@
+# -*- coding:utf-8 -*-
+# author: xip tim:2018/4/3
+from flask_sqlalchemy import SQLAlchemy
+
+db = SQLAlchemy()
